2d64d52e1480a2662121124448d26db3de930a55
Shreys-Git <write.shrey@gmail.com>
2023-07-07 18:08:54 +0530

    updated docker compose

--- client/src/components/ItemSelector.js
+++ client/src/components/ItemSelector.js
@@ -6,10 +6,9 @@ export const ItemSelector = () => {
 
   const fetchProducts = async () => {
     try {
-      const response = await fetch("https://b0aa-182-48-213-9.in.ngrok.io");
+      const response = await fetch("https://fakestoreapi.com/products/");
       const products = await response.json();
-      const title = products[0];
-      console.log("Fetched Data: ", products);
+      const title = products[0].title;
       setProduct(title);
     } catch (error) {
       console.log("Error fetching products:", error);
@@ -41,6 +40,7 @@ export const ItemSelector = () => {
   return (
     <>
       <button onClick={saveProduct}>{product}</button>
+      <br></br>
       <br></br>
       {savedId}
     </>
--- server/main.py
+++ server/main.py
@@ -1,7 +1,43 @@
+# from pymongo import MongoClient
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# from pydantic import BaseModel
+
+# app = FastAPI()
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# # client = MongoClient("mongodb://localhost:27017/")
+# client = MongoClient("mongodb://mongo:27017/")
+
+# db = client["mockdb"]
+# collection = db["mockcollection"]
+
+# class Product(BaseModel):
+#     product: str
+
+# @app.get("/")
+# async def welcome():
+#     return {"Message": "Welcome"}
+
+# @app.post("/")
+# async def save_product(item: Product):
+#     product_name = item.product
+#     data = {"name": product_name}
+#     result = collection.insert_one(data)
+#     return {"message": str(result.inserted_id)}
+
 from pymongo import MongoClient
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import os
 
 app = FastAPI()
 
@@ -13,8 +49,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-client = MongoClient("mongodb://localhost:27017/")
-# client = MongoClient("mongodb://mongo:27017/")
+DB_URL = os.getenv("DB_URL")
+
+
+client = MongoClient(DB_URL)
 
 db = client["mockdb"]
 collection = db["mockcollection"]
@@ -32,4 +70,3 @@ async def save_product(item: Product):
     data = {"name": product_name}
     result = collection.insert_one(data)
     return {"message": str(result.inserted_id)}
-
